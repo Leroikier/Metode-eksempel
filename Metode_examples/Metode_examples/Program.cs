d3a0b0eef7a446266fcb3e5ef0e1b5ff789f5b34
Alexander Roikier Møller <alex6308@elev.tec.dk>
2018-08-22 08:48:21 +0200

    <Færdigt resultat med kommentar>

--- Metode_examples/Metode_examples/Program.cs
+++ Metode_examples/Metode_examples/Program.cs
@@ -11,31 +11,43 @@ namespace Metode_examples
         static void Main(string[] args)
         {
             //Variabel
-            string navn;
             string totalNavn = "";
+            string totalNavn1 = "";
             //Input
-            Console.WriteLine("Hvad hedder du : ");
-            navn = Console.ReadLine().Trim();
+            Console.Write("Hvad hedder du : ");
+            string navn = Console.ReadLine().Trim();
+            Console.Write("Hvad hedder din far : ");
+            string fnavn = Console.ReadLine().Trim();
 
             //et string array oprettes via "navn", som splitter navn efter hvert mellemrum man indtaster
+            //Mellemrum er default = ();
             string[] delNavn = navn.Split();
+            string[] delNavn1 = fnavn.Split();
 
             //For hver string "tekst" i delNavn udskriv "xnavn" med metodens parametre inkluderet
             foreach (string xnavn in delNavn)
             {
                 totalNavn += StortOgSmaat(xnavn); //Sætter alle navnene sammen 
             }
+            foreach (string xnavn in delNavn1)
+            {
+                totalNavn1 += StortOgSmaat(xnavn); //Sætter alle navnene sammen 
+            }
+            //Udskrivning /TrimEnd sørger for at fjerne mellemrum i udskrivningen
             Console.WriteLine(totalNavn.TrimEnd());
-            Console.ReadLine();
+            Console.WriteLine(totalNavn1.TrimEnd());
 
             //Her oprettes en metode i class "main", som sørger for at det første bokstav er stort og resten bliver småt
             string StortOgSmaat(string inavn)
             {
+                //hvis inavn ikke er 0 køre den metoden under
                 if (inavn.Length != 0)
                     return inavn.Substring(0, 1).ToUpper() + inavn.Substring(1).ToLower() + " ";
                 else
+                    //ellers returnere "tomt"
                     return "";
             }
+            Console.ReadKey();
         }
     }
 }
